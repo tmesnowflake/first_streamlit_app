56559bf74b34819c6c9018d1e4e88716fa0904b5
tmesnowflake <114143694+tmesnowflake@users.noreply.github.com>
2022-09-22 10:01:53 +0100

    Update streamlit_app.py
    Added additional items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 # Streamlit app 
-import streamlit 
+import streamlit as sl
 
-streamlit.title('Parents new healthy dinner')
+sl.title('Parents new healthy dinner')
+
+sl.headder('Brekfast Favourites')
+sl.text('🥣 Omega3 & Blueberry Waffles')
+sl.text('🥗 Kale, Spinach & rocket smoothie')
+sl.text('🐔 Boiled Free Range egg')
+sl.text('🥑 Avo on toast')
+
+sl.header('Build your own smoothie')
