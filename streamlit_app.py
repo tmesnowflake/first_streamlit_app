423de3409a9e6d059508e4154c144be56cfd5131
tmesnowflake <114143694+tmesnowflake@users.noreply.github.com>
2022-09-23 12:14:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Streamlit app 
 import streamlit as sl
 import pandas as pd 
+import requests
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -17,3 +18,7 @@ fruit_selected  = sl.multiselect ('Pick your fruit: ', list(my_fruit_list.index)
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 
 sl.dataframe(fruit_to_show)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
